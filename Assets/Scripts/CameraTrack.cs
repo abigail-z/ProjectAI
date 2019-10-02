33c7960c53c988a6017f00a63979ae882bdb74fd
Nathaniel Zeeman <NathanielZeeman@gmail.com>
2019-10-02 16:19:39 -0700

    first commit

---
+++ Assets/Scripts/CameraTrack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrack : MonoBehaviour
+{
+    public Transform toTrack;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
