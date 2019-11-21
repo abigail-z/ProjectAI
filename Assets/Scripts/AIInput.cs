ff4f8b2ed8f90ed4a7ba22a0fd253ab4090aa88e
Nathaniel Zeeman <nathanielzeeman@gmail.com>
2019-11-21 00:38:34 -0800

    added path following. has trouble with the collision on the sides of the tracks

--- Assets/Scripts/AIInput.cs
+++ Assets/Scripts/AIInput.cs
@@ -6,13 +6,69 @@ public class AIInput : MonoBehaviour
 {
     public Path path;
     public Transform car;
+    private CarBehaviour behaviour;
+    private LayerMask wallMask;
+    private Rigidbody sphere;
+
+    void Awake()
+    {
+        behaviour = GetComponent<CarBehaviour>();
+        wallMask = LayerMask.GetMask("Wall");
+        sphere = transform.Find("Sphere").GetComponent<Rigidbody>();
+    }
 
     void FixedUpdate()
     {
+        // always accelerate, assume no turn
+        behaviour.accelerationInput = 1;
+        behaviour.turnInput = 0;
+
+        // find path goal point and vector perpendicular to track direction
         PathPointInfo ppi = path.FindClosestLeadingPoint(car.position, 5);
+        Vector3 perpendicular = new Vector3(ppi.direction.z, 0, -ppi.direction.x);
+
+        // grab the car's origin point to raycast from
+        Vector3 carPos = car.position;
+        carPos.y = ppi.point.y;
+
+
+        // find the intersection between the car's heading and perpendicular to the track
+        Vector3 intersect = GetLineIntersectionPoint(carPos, carPos + car.forward, ppi.point, ppi.point + perpendicular, out bool found);
+        if (found)
+        {
+            // raycast to the intersect between the goal line and forward line
+            intersect.y = ppi.point.y;
+            float maxDistance = (intersect - carPos).magnitude;
+            if (Physics.SphereCast(carPos, 1, intersect - carPos, out RaycastHit _, maxDistance, wallMask))
+            {
+                // going to hit a wall, time to correct
+                // turn toward the goal point
+                float dir = Vector3.Dot(car.right, ppi.point - carPos);
+                if (dir != 0)
+                {
+                    behaviour.turnInput = dir / Mathf.Abs(dir);
+                    return;
+                }
+            }
+        }
+        else
+        {
+            // raycast the max distance
+            if (Physics.SphereCast(carPos, 1, car.forward, out RaycastHit _, 8, wallMask))
+            {
+                // going to hit a wall, time to correct
+                // turn toward the goal point
+                float dir = Vector3.Dot(car.right, ppi.point - carPos);
+                if (dir != 0)
+                {
+                    behaviour.turnInput = dir / Mathf.Abs(dir);
+                    return;
+                }
+            }
+        }
     }
 
-    public Vector3 GetIntersectionPoint(Vector3 A1, Vector3 A2, Vector3 B1, Vector3 B2, out bool found)
+    public Vector3 GetLineIntersectionPoint(Vector3 A1, Vector3 A2, Vector3 B1, Vector3 B2, out bool found)
     {
         float tmp = (B2.x - B1.x) * (A2.z - A1.z) - (B2.z - B1.z) * (A2.x - A1.x);
 
@@ -40,7 +96,7 @@ public class AIInput : MonoBehaviour
         PathPointInfo ppi = path.FindClosestLeadingPoint(car.position, 5);
         Vector3 perpendicular = new Vector3(ppi.direction.z, 0, -ppi.direction.x);
 
-        Vector3 intersect = GetIntersectionPoint(car.position, car.position + car.forward, ppi.point, ppi.point + perpendicular, out bool found);
+        Vector3 intersect = GetLineIntersectionPoint(car.position, car.position + car.forward, ppi.point, ppi.point + perpendicular, out bool found);
         if (found)
         {
             intersect.y = ppi.point.y;
@@ -52,6 +108,7 @@ public class AIInput : MonoBehaviour
         Gizmos.color = Color.white;
         Gizmos.DrawWireSphere(ppi.point, 0.25f);
         Gizmos.DrawLine(ppi.point, ppi.point + ppi.direction);
+        Gizmos.DrawWireSphere(car.position, 1.5f);
     }
 #endif
 }
