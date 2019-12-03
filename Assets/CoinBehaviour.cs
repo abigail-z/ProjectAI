54885143c98c73e60d9c05f9dbe38fa218df26cd
Nathaniel Zeeman <NathanielZeeman@gmail.com>
2019-12-03 13:13:37 -0800

    created rotating coin, does nothing

---
+++ Assets/CoinBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBehaviour : MonoBehaviour
+{
+    public Vector3 spinSpeed;
+
+    void Update()
+    {
+        transform.Rotate(Time.deltaTime * spinSpeed);
+    }
+}
