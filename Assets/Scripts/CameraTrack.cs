937a2167d82c64319d291df53e9b67ef6e86c917
Nathaniel Zeeman <nathanielzeeman@gmail.com>
2019-12-05 01:29:38 -0800

    fixed camera offset and applied prefab fixes

--- Assets/Scripts/CameraTrack.cs
+++ Assets/Scripts/CameraTrack.cs
@@ -10,17 +10,19 @@ public class CameraTrack : MonoBehaviour
 
     private Vector3 offset;
     private Vector3 refVelocity;
+    private float startY;
 
     void Start()
     {
         offset = transform.position - toTrack.position;
+        startY = transform.position.y;
     }
 
     void LateUpdate()
     {
         Vector3 predictedPos = toTrack.position + toTrack.velocity * predictionTime;
         Vector3 newPos = Vector3.SmoothDamp(transform.position, predictedPos + offset, ref refVelocity, maxFollowTime);
-        newPos.y = offset.y;
+        newPos.y = startY;
         transform.position = newPos;
     }
 }
